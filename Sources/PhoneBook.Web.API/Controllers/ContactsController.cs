de51b2fd7ceb8c35372d5406b47895058801c5ea
agrynco <agrynco@gmail.com>
2017-09-27 19:20:04 +0300

    Created an API project

---
+++ Sources/PhoneBook.Web.API/Controllers/ContactsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using PhoneBook.Domain;
+
+namespace PhoneBook.Web.API.Controllers
+{
+    [RoutePrefix("contacts")]
+    public class ContactsController : ApiController
+    {
+        [Route]
+        public Contact[] Get()
+        {
+            return new Contact[0];
+        }
+    }
+}
